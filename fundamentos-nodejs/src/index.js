44ea48ce2d21ffff9eb970755a822762fa04aba3
DevGabrielPires <gabriel.r.pires@outlook.com>
2021-12-11 16:46:56 -0300

    :sparkles: Feat: finishing class about methods http

--- fundamentos-nodejs/src/index.js
+++ fundamentos-nodejs/src/index.js
@@ -4,8 +4,24 @@ const app = express();
 
 const port = 3333;
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'hello wordl ignite' });
+app.get('/courses', (request, response) => {
+  return response.json(['curso 1', 'curso 2', 'curso 3']);
+});
+
+app.post('/courses', (request, response) => {
+  return response.json(['curso 1', 'curso 2', 'curso 3', 'curso 4']);
+});
+
+app.put('/courses/:id', (request, response) => {
+  return response.json(['curso 6', 'curso 2', 'curso 3', 'curso 4']);
+});
+
+app.patch('/courses/:id', (request, response) => {
+  return response.json(['curso 6', 'curso 6', 'curso 3', 'curso 4']);
+});
+
+app.patch('/courses/:id', (request, response) => {
+  return response.json(['curso 6', 'curso 6', 'curso 4']);
 });
 
 app.listen(port);

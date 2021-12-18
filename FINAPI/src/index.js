898de71f4be230fc9a2911ad6b79a8bda1929fae
DevGabrielPires <gabriel.r.pires@outlook.com>
2021-12-18 15:52:22 -0300

    :sparkles: Feat: finishing class about cadastro

--- FINAPI/src/index.js
+++ FINAPI/src/index.js
@@ -1,6 +1,27 @@
 const express = require('express');
 
+const { v4: uuidV4 } = require('uuid');
+
 const app = express();
+
+app.use(express.json());
+
+const custumers = [];
+
+app.post('/account', (request, response) => {
+  const { cpf, name } = request.body;
+  const id = uuidV4();
+
+  custumers.push({
+    cpf,
+    name,
+    id,
+    statement: [],
+  });
+
+  console.log(custumers);
+  return response.status(201).send();
+});
 
 const port = 3333;
 
